fbcb3cc9e86cf1dfae179b97d7c255c03d206b41
Leveque <rleveque@ptc.com>
2020-03-12 19:54:56 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,167 @@
+const {src, dest,series, watch} = require('gulp');
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
+    concat = require('gulp-concat');
+
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    outputDir,
+    sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+
+coffeeSources = ['components/coffee/tagline.coffee'];
+jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
+];
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+jsonSources = [outputDir + 'js/*.json'];
+
+
+function coffeeMaker(cb) {
+  src(coffeeSources)
+    .pipe(coffee({ bare: true }).on('error', gutil.log))
+    .pipe(dest('components/scripts'))
+  cb();
+  }
+
+/**gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+    .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
+});*/
+
+
+function js(cb) {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
+  cb();
+  }
+
+/**gulp.task('js', function() {
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulpif(env === 'production', uglify()))
+    .pipe(gulp.dest(outputDir + 'js'))
+    .pipe(connect.reload())
+});*/
+
+function compassMaker(cb) {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: outputDir + 'images',
+      style: sassStyle
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload())
+  cb();
+}
+/**gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: outputDir + 'images',
+      style: sassStyle
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload())
+});*/
+
+/**gulp.task('watch', function() {
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(jsSources, ['js']);
+  gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch('builds/development/*.html', ['html']);
+  gulp.watch('builds/development/js/*.json', ['json']);
+});*/
+
+function watcher(cb) {
+  watch(coffeeSources).on('change', series(coffee))
+  watch(jsSources).on('change', series(js))
+  watch('components/sass/*.scss').on('change', series(compass))
+  watch('builds/development/*.html').on('change', series(html))
+  watch('builds/development/js/*.json').on('change', series(json))
+  watch('builds/development/images/**/*.*').on('change', series(imagesReducer))
+  cb();
+}
+
+
+/**gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});**/
+function connectServer(cb) {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+  cb();
+}
+
+function html(cb) {
+  src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload());
+  cb();
+}
+
+function imagesReducer(cb) {
+  src('builds/development/images/**/*.*')
+    .pipe(gulpif(env === 'production', imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngcrush()]
+    })))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+    .pipe(connect.reload());
+  cb();
+}
+
+function json(cb) {
+  src('builds/development/js/*.json')
+    .pipe(gulpif(env === 'production', jsonminify()))
+    .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
+    .pipe(connect.reload());
+  cb();
+}
+
+//gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+exports.default = series(html, json, coffeeMaker, js, compassMaker, imagesReducer, connectServer, watcher);
